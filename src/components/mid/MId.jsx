6a970b73bd6ab27ba6b404beeeb60d03822edce7
Prince Gupta <princegupta1586@gmail.com>
2023-01-25 23:42:44 +0530

    Created Fetch API Functionality

--- src/components/mid/MId.jsx
+++ src/components/mid/MId.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import React from "react";
 import "./Mid.css";
 
 export default function MId() {
+  const [query, setQuery] = useState("");
+  const [apiData, setApiData] = useState(null);
+
+  const getData = async (query) => {
+    const APP_ID = import.meta.env.VITE_APPLICATION_ID;
+    const APP_KEY = import.meta.env.VITE_APPLICATION_KEY;
+    const q = query;
+    let url = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=${q}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setApiData(data);
+      });
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+    // console.log(query);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    // console.log(query);
+    getData(query);
+  };
+
   return (
     <div className="mid">
       <div className="midContainer">
@@ -10,10 +39,26 @@ export default function MId() {
         </div>
         <div className="form">
           <form>
-            <input type="text" placeholder="Search Recipe" />
-            <button type="button">Search</button>
+            <input
+              type="text"
+              id="query"
+              name="query"
+              placeholder="Search Recipe"
+              onChange={handleChange}
+              value={query}
+            />
+            <button type="button" onClick={handleClick}>
+              Search
+            </button>
           </form>
         </div>
+        {/* <div className="recipeCards">
+          { !apiData.more ? (
+            <div>Recipe Not Found</div>
+          ): (apiData.hits.map(recipe => {
+
+          }))}
+        </div> */}
       </div>
     </div>
   );
